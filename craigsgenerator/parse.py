5881177faab4c1d8f1ba2ea6e94a045d60a5f2ee
Thomas Levine <_@thomaslevine.com>
2014-02-16 17:10:55 +0000

    parse row

---
+++ craigsgenerator/parse.py
@@ -0,0 +1,19 @@
+SEARCH_ROW_ATTRS = [
+    ('href','a/@href'),
+    ('latitude', '@data-latitude'),
+    ('longitude', '@data-longitude'),
+    ('date','
+]
+
+def search_row(p):
+    'Parse a <p class="row"></p>.'
+    row = {}
+    for key, attr in SEARCH_ROW_ATTRS:
+        results = p.xpath(attr)
+        if len(results) == 1:
+            row[key] = attr
+    return row
+
+
+ <p class="row" data-latitude="30.174300" data-longitude="-97.822500" data-pid="4328170456">
+    <a href="/sub/4328170456.html" class="i" data-id="0:00V0V_bJm3ol0z8rY"></a> <span class="star"></span> <span class="pl"> <span class="date">Feb 15</span>  <a href="/sub/4328170456.html">**Exceptional LIVING - 2bd/ 2ba LUXURY</a> </span> <span class="l2">  <span class="price">&#x0024;1400</span> / 2br - 1230ft&sup2; -  <span class="pnr"> <small> (austin)</small> <span class="px"> <span class="p"> pic&nbsp;<a href="#" class="maptag" data-pid="4328170456">map</a></span></span> </span>  </span> </p>
