4b2b85643525cd353f938d1efb51e9f6eca23ab3
Thomas Levine <_@thomaslevine.com>
2014-02-17 22:19:56 +0000

    rst

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from distutils.core import setup
 
+def read(fn):
+    with open(fn) as file:
+        out = file.read()
+    return out
+
 setup(name='craigsgenerator',
     author='Thomas Levine',
     author_email='_@thomaslevine.com',
     description='Iterate through Craig\'s list',
+ #  long_description = read('README'),
     url='https://github.com/tlevine/craigsgenerator.git',
     classifiers=[
         'Intended Audience :: Developers',
