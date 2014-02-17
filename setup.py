875e767e201a492f95828d280c2a71a993b653d9
Thomas Levine <_@thomaslevine.com>
2014-02-17 22:07:44 +0000

    change rst

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from distutils.core import setup
 
-def read(fn):
-    with open(fn) as file:
-        out = file.read()
-    return out
-
 setup(name='craigsgenerator',
     author='Thomas Levine',
     author_email='_@thomaslevine.com',
     description='Iterate through Craig\'s list',
-    long_description = read('README.rst'),
     url='https://github.com/tlevine/craigsgenerator.git',
     classifiers=[
         'Intended Audience :: Developers',
