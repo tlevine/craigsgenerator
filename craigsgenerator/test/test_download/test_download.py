b66f624721f2ae0417ef119ec8ecdbabe57f7dc8
Thomas Levine <_@thomaslevine.com>
2014-03-03 18:24:39 +0000

    yay

--- craigsgenerator/test/test_download/test_download.py
+++ craigsgenerator/test/test_download/test_download.py
@@ -10,7 +10,7 @@ FakeResponse = namedtuple('Response', ['text'])
 def fake_get(url):
     return FakeResponse('lalala')
 
-fake_warehouse = {'http://foo.bar': 'baz'}
+fake_warehouse = {('http://foo.bar','2014/08'): FakeResponse('baz')}
 fake_date = datetime.date(2014,3,1)
 
 def test_bad_scheme():
