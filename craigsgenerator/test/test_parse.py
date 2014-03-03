67df1cb8efe2f0315d5bcc13ce90caedd596c147
Thomas Levine <_@thomaslevine.com>
2014-03-03 17:32:45 +0000

    start testing

--- craigsgenerator/test/test_parse.py
+++ craigsgenerator/test/test_parse.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 import lxml.html
 
 import craigsgenerator.parse as parse
@@ -6,6 +8,8 @@ p = lxml.html.fromstring('''
  <p class="row" data-latitude="30.289600" data-longitude="-97.739600" data-pid="4331364900"> <a href="/sub/4331364900.html" class="i"></a> <span class="star"></span> <span class="pl"> <span class="date">Feb 15</span>  <a href="/sub/4331364900.html">Pre-leasing Apt in WEST CAMPUS for Summer 2014</a> </span> <span class="l2">  <span class="price">&#x0024;925</span> / 1br -  <span class="pnr"> <small> (806 West 24th St)</small> <span class="px"> <span class="p"> <a href="#" class="maptag" data-pid="4331364900">map</a></span></span> </span>  </span> </p>
 ''')
 p.make_links_absolute('https://austin.craigslist.org/sub/index200.html')
+
+FakeResponse = namedtuple('Response', ['text'])
 
 def test_search_row_with_location():
     o = parse.search_row(p)
@@ -18,3 +22,10 @@ def test_search_row_with_location():
         'price': 925,
     }
     assert o == e
+
+def test_search():
+    fn = 'austin-sub.html'
+    with open(os.path.join('craigsgenerator','test','fixtures',fn)) as fp:
+        r = FakeResponse(fp.read())
+    observed = parse.search(r)
+    n.assert_equal(len(observed), 3)
