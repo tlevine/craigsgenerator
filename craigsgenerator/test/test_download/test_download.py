9ef6a11d491515047ec5ac164fe4c6f991a2e0de
Thomas Levine <_@thomaslevine.com>
2014-03-03 18:30:37 +0000

    test_download

--- craigsgenerator/test/test_download/test_download.py
+++ craigsgenerator/test/test_download/test_download.py
@@ -1,28 +1,18 @@
-from collections import namedtuple
-import datetime
-
 import nose.tools as n
 
 from craigsgenerator.download import download
-
-FakeResponse = namedtuple('Response', ['text'])
-
-def fake_get(url):
-    return FakeResponse('lalala')
-
-fake_warehouse = {('http://foo.bar','2014/08'): FakeResponse('baz')}
-fake_date = datetime.date(2014,3,1)
+import craigsgenerator.test.test_download.utils as utils
 
 def test_bad_scheme():
     with n.assert_raises(ValueError):
-        r = download(fake_get, fake_warehouse, 'ftp://example.com', fake_date)
+        r = download(utils.fake_get, utils.fake_warehouse, 'ftp://example.com', utils.fake_date)
 
 def test_cached():
-    r = download(fake_get, fake_warehouse, 'http://foo.bar', fake_date)
+    r = download(utils.fake_get, utils.fake_warehouse, 'http://foo.bar', utils.fake_date)
     n.assert_equal(r.text, 'baz')
 
 def test_not_cached():
     d = {}
-    r = download(fake_get, d, 'http://foo.bar', fake_date)
+    r = download(utils.fake_get, d, 'http://foo.bar', utils.fake_date)
     n.assert_equal(r.text, 'lalala')
-    n.assert_dict_equal(d, {('http://foo.bar', '2014/08'): FakeResponse('lalala')})
+    n.assert_dict_equal(d, {('http://foo.bar', '2014/08'): utils.FakeResponse('lalala')})
--- craigsgenerator/test/test_download/test_download_many.py
+++ craigsgenerator/test/test_download/test_download_many.py
@@ -1,4 +1,19 @@
 import nose.tools as n
 
-from craigsgenerator.download import
+from craigsgenerator.download import download_many
+
+def fake_get(url):
+    return FakeResponse('lalala')
+
+fake_warehouse = {('http://foo.bar','2014/08'): FakeResponse('baz')}
+fake_date = datetime.date(2014,3,1)
+
+def test_cached(get, warehouse, urls, date_func, n_threads):
+
+def test_bad_scheme():
+    with n.assert_raises(ValueError):
+        r = download(fake_get, fake_warehouse, 'ftp://example.com', fake_date)
+
+def test_cached():
+    r = download(fake_get, fake_warehouse, 'http://foo.bar', fake_date)
 
