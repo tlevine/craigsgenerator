a6da87a511ab8564077ced70f132c67ec597ad93
Thomas Levine <_@thomaslevine.com>
2014-03-03 20:19:07 +0000

    expose params differently

--- craigsgenerator/__init__.py
+++ craigsgenerator/__init__.py
@@ -1,24 +1,30 @@
+import requests
 from queue import Queue
 from craigsgenerator.generators import listings as _listings
 from craigsgenerator.generators import sites as _sites
 
-def craigsgenerator(sites = _sites, listings = _listings, sections = ['sub'], threaded = True):
+def craigsgenerator(cachedir = 'craigslist', scheme = 'https', get = requests.get,
+                    date_func = datetime.date, threads_per_section = 10, superthreaded = True,
+                    sites = _sites, listings = _listings, sections = lambda: ['sub']):
+    '''
+    '''
+
     results = Queue()
     def worker(thesite, thesection):
         for listing in thelistings(thesite, thesection):
             results.put(listing)
 
-    if threaded:
+    if superthreaded:
         threads = {}
 
     for site in sites():
         for section in sections:
-            if threaded:
+            if superthreaded:
                 threads[(site, section)] = Thread(None, worker, args = (site, section))
             else:
                 worker(thesite, thesection)
 
-    if threaded:
+    if superthreaded:
         for thread in threads.values():
             thread.start()
         for thread in threads.values():
