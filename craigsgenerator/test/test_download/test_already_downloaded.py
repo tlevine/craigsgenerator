d25d2ae58bfc87159c06242902f76290d49244ee
Thomas Levine <_@thomaslevine.com>
2014-03-03 19:00:59 +0000

    fix the test

--- craigsgenerator/test/test_download/test_already_downloaded.py
+++ craigsgenerator/test/test_download/test_already_downloaded.py
@@ -1,12 +1,15 @@
+import datetime
+
 import nose.tools as n
 
 from craigsgenerator.download import already_downloaded
 
 url = 'http://thomaslevine.com/!'
-date = '2013/02'
+date = datetime.date(2013, 2, 1)
+datestring = '2013/04'
 
 def test_yes():
-    warehouse = {(url, date): 'this string doesn\'t matter.'}
+    warehouse = {(url, datestring): 'this string doesn\'t matter.'}
     n.assert_true(already_downloaded(warehouse, url, date))
 
 def test_no():
