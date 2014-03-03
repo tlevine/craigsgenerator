4e86d5fc5083d902c3643bb98a5e88aa7d62c250
Thomas Levine <_@thomaslevine.com>
2014-03-03 18:57:04 +0000

    yay

--- craigsgenerator/test/test_download/test_threaded_download_worker.py
+++ craigsgenerator/test/test_download/test_threaded_download_worker.py
@@ -5,13 +5,15 @@ import nose.tools as n
 from craigsgenerator.download import threaded_download_worker
 import craigsgenerator.test.test_download.utils as utils
 
+url = 'http://blah'
+content = 'zombies'
 
 def test():
-    warehouse = {('http://blah', utils.fake_datestring): 'zombies'}
+    warehouse = {(url, utils.fake_datestring): content}
     queue = Queue()
     threaded_download_worker(utils.fake_get_should_not_run,
-            warehouse, 'http://blah', utils.fake_date_func, queue)
+            warehouse, url, utils.fake_date_func, queue)
     print(warehouse)
     n.assert_false(queue.empty())
-    n.assert_equal(queue.get(), 'zombies')
+    n.assert_equal(queue.get(), (url, content))
     n.assert_true(queue.empty())
