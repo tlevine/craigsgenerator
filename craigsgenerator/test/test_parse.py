48b6387abe2b29caef30267c3072e4cc175de5d5
Thomas Levine <_@thomaslevine.com>
2014-03-06 07:10:22 +0000

    aeou

--- craigsgenerator/test/test_parse.py
+++ craigsgenerator/test/test_parse.py
@@ -56,17 +56,17 @@ def test_listing():
     for fn, expected in listing_testcases:
         yield check_listing, fn, expected
 
-def check_next_search_url(fn, url):
+def check_next_search_url(fn, domain, url):
     with open(os.path.join('craigsgenerator','test','fixtures',fn)) as fp:
         html = lxml.html.fromstring(fp.read())
     html.make_links_absolute(url)
-    observed = parse.next_search_url('https', 'austin.craigslist.org', 'sub', html)
+    observed = parse.next_search_url('https', domain, 'sub', html)
     n.assert_equal(observed, url)
 
 def test_next_search_url():
     testcases = [
-        ('austin-sub.html', 'https://austin.craigslist.org/sub/index100.html'),
-        ('chicago-sub.html', 'https://austin.craigslist.org/sub/index100.html'),
+        ('austin-sub.html', 'austin.craigslist.org', 'https://austin.craigslist.org/sub/index100.html'),
+        ('chicago-sub.html', 'chicago.craigslist.org', 'https://chicago.craigslist.org/sub/index100.html'),
     ]
-    for fn, url in testcases:
-        yield check_next_search_url, fn, url
+    for fn, domain, url in testcases:
+        yield check_next_search_url, fn, domain, url
