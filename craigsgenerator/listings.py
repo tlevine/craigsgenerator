98845f778d44d839b5f40ea6c64b71859e9a5223
Thomas Levine <_@thomaslevine.com>
2014-03-08 20:18:03 +0000

    oeu

--- craigsgenerator/listings.py
+++ craigsgenerator/listings.py
@@ -13,7 +13,8 @@ from pickle_warehouse import Warehouse
 import craigsgenerator.download as download
 import craigsgenerator.parse as parse
 
-def listings(scheme, get, n_threads, warehouse, site, section):
+def listings(scheme, get, n_threads, warehouse, site, section,
+             parse_listing, parse_search, parse_next_search_url):
     '''
     Generate listings.
 
@@ -32,7 +33,7 @@ def listings(scheme, get, n_threads, warehouse, site, section):
             responses = download.download_many(warehouse, urls, get, n_threads, download.threaded_download_worker)
             for response in responses:
                 result = urls[response.url]
-                result.update(parse.listing(response))
+                result.update(parse_listing(response))
 
                 result['html'] = response.text
                 result['downloaded'] = datetime_func()
@@ -46,11 +47,11 @@ def listings(scheme, get, n_threads, warehouse, site, section):
             results = []
 
             # Search
-            url = parse.next_search_url(scheme, site, section, html)
+            url = parse_next_search_url(scheme, site, section, html)
             if url == None:
                 break
             response = download.download(warehouse, url, get)
-            results = parse.search(response)
+            results = parse_search(response)
             html = lxml.html.fromstring(response.text)
             html.make_links_absolute(url)
             if results == []:
--- craigsgenerator/test/test_listings.py
+++ craigsgenerator/test/test_listings.py
@@ -3,20 +3,31 @@ import nose.tools as n
 import craigsgenerator.test.util as util
 from craigsgenerator.listings import listings
 
-fake_response = lambda url: util.FakeResponse(url, '<a href="%s">Look!</a>' % url)
+fake_response = lambda url: util.FakeResponse(url = url, text = '<a href="%s">Look!</a>' % url)
 
 def test_listings():
     scheme = 'https'
     gotten = []
     def get(url):
         gotten.append(url)
-        return fake_response
+        return fake_response(url)
     n_threads = 4
     warehouse = {}
     site = 'chicago.craigslist.org'
     section = 'sub'
 
-    l = listings(scheme, get, n_threads, warehouse, site, section)
+    parse_listing = lambda _: {}
+    parse_search = lambda _: [{'href':None,'date' :None}]
+
+    def parse_next_search_url(scheme, site, section, html, searched_urls = 0):
+        if html == None:
+            searched_urls *= 0
+        url = '%s://%s/%s/index%03d.html' % (scheme, site, section, searched_urls)
+        searched_urls += 1
+        return url
+
+    l = listings(scheme, get, n_threads, warehouse, site, section,
+                 parse_listing, parse_search, parse_next_search_url)
     n.assert_list_equal(gotten, [])
 
     response = next(l)
