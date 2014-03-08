23729b6a40d287e8cf18f9c0f28a59555caaa0c5
Thomas Levine <_@thomaslevine.com>
2014-03-08 21:31:22 +0000

    download with date

--- craigsgenerator/test/test_download/test_download.py
+++ craigsgenerator/test/test_download/test_download.py
@@ -5,14 +5,18 @@ import craigsgenerator.test.test_download.utils as utils
 
 def test_bad_scheme():
     with n.assert_raises(ValueError):
-        r = download(utils.fake_warehouse, 'ftp://example.com', utils.fake_get)
+        r = download(utils.fake_warehouse, 'ftp://example.com', utils.fake_get, None)
 
 def test_cached():
-    r = download(utils.fake_warehouse, 'http://foo.bar', utils.fake_get)
+    r = download(utils.fake_warehouse, 'http://foo.bar', utils.fake_get, None)
+    n.assert_equal(r.text, 'baz')
+
+def test_cached_withdate():
+    r = download(utils.fake_warehouse, 'http://foo.bar', utils.fake_get, '2014-03-01')
     n.assert_equal(r.text, 'baz')
 
 def test_not_cached():
     d = {}
-    r = download(d, 'http://foo.bar', utils.fake_get)
+    r = download(d, 'http://foo.bar', utils.fake_get, None)
     n.assert_equal(r.text, 'lalala')
     n.assert_dict_equal(d, {'http://foo.bar': utils.fake_get(None)})
--- craigsgenerator/test/test_download/utils.py
+++ craigsgenerator/test/test_download/utils.py
@@ -11,5 +11,7 @@ def fake_get_should_not_run(_):
 
 fake_warehouse = {'http://foo.bar': FakeResponse('http://thomaslevine.com', 'baz')}
 
+fake_warehouse_with_date = {('http://foo.bar','2014-03-01'): FakeResponse('http://thomaslevine.com', 'baz')}
+
 def fake_date_func():
     return fake_date
