43538416175329616d4a4bfe7e3c543e3cffa352
Thomas Levine <_@thomaslevine.com>
2014-03-08 21:22:55 +0000

    oeu

--- craigsgenerator/test/test_listings.py
+++ craigsgenerator/test/test_listings.py
@@ -6,7 +6,7 @@ import craigsgenerator.test.util as util
 from craigsgenerator.listings import listings, _join
 
 fake_response = lambda url: util.FakeResponse(url = url, text = '<a href="%s">Look!</a>' % url)
-fake_datetime = datetime.datetime(2014,1,1)
+fake_datetime = datetime.datetime(2014,2,1)
 
 def fake_result(url):
     return {
@@ -49,7 +49,6 @@ def test_listings():
     n.assert_set_equal(gotten, set())
 
     result = next(l)
-    (result, fake_result('https://chicago.craigslist.org/sub/index000.html'))
     n.assert_equal(result, fake_result('https://chicago.craigslist.org/sub/index000.html'))
     n.assert_set_equal(gotten, {'https://chicago.craigslist.org/sub/index000.html'})
 
@@ -72,3 +71,30 @@ def test_join():
                 'html': '<html></html>', 'downloaded': datetime_func()}
     n.assert_dict_equal(search_row, {'href': url, 'date': '3 months ago'}) # should not mutate
     n.assert_dict_equal(observed, expected)
+
+def test_cache():
+    scheme = 'https'
+    gotten = set()
+    def get(url):
+        raise AssertionError('This should not run.')
+    n_threads = 4
+    url = 'https://chicago.craigslist.org/sub/index000.html'
+    warehouse = {(url,2014,4):fake_response(url)}
+    site = 'chicago.craigslist.org'
+    section = 'sub'
+
+    parse_listing = lambda response: {'html':response.text,'foo':'bar'}
+    parse_search = lambda response: [{'href':response.url, 'date': None}]
+
+    searched = set()
+    def parse_next_search_url(scheme, site, section, html):
+        if html == None:
+            searched.clear()
+        url = '%s://%s/%s/index%d00.html' % (scheme, site, section, len(searched))
+        searched.add(url)
+        return url
+
+    l = listings(scheme, get, n_threads, warehouse, site, section,
+                 parse_listing, parse_search, parse_next_search_url,
+                 fake_download_many, None, lambda: fake_datetime)
+    n.assert_dict_equal(next(l), fake_result(url))
