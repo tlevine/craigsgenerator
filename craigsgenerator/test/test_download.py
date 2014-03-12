8f92f61f817eb34741a4edf3d3c403595bf52971
Thomas Levine <_@thomaslevine.com>
2014-03-12 20:04:16 +0000

    yay

---
+++ craigsgenerator/test/test_download.py
@@ -0,0 +1,53 @@
+import nose.tools as n
+from collections import namedtuple
+import datetime
+
+from craigsgenerator.download import download_one, download_many
+
+FakeResponse = namedtuple('Response', ['url', 'text'])
+
+def fake_get(url):
+    return FakeResponse('http://chainsaw.thomaslevine.com', 'lalala')
+
+def fake_get_should_not_run(_):
+    raise AssertionError('This should not run.')
+
+fake_warehouse = {'http://foo.bar': FakeResponse('http://thomaslevine.com', 'baz')}
+
+fake_warehouse_with_date = {('http://foo.bar','2014-03-01'): FakeResponse('http://thomaslevine.com', 'baz')}
+
+def fake_date_func():
+    return fake_date
+
+def check_bad_scheme(download):
+    with n.assert_raises(ValueError):
+        r = download(fake_warehouse, 'ftp://example.com', fake_get, None)
+
+def check_cached(download):
+    r = download(fake_warehouse, 'http://foo.bar', fake_get, None)
+    n.assert_equal(r.text, 'baz')
+
+def check_cached_with_date(download):
+    r = download(fake_warehouse_with_date, 'http://foo.bar', fake_get_should_not_run, '2014-03-01')
+    n.assert_equal(r.text, 'baz')
+    r = download(fake_warehouse_with_date, 'http://foo.bar', fake_get, '2014-03-02')
+    n.assert_equal(r.text, 'lalala')
+
+def check_not_cached(download):
+    d = {}
+    r = download(d, 'http://foo.bar', fake_get, None)
+    n.assert_equal(r.text, 'lalala')
+    n.assert_dict_equal(d, {'http://foo.bar': fake_get(None)})
+
+def test_bad_scheme():
+    for download in download_one, download_many
+        yield check_bad_scheme, download
+def test_cached():
+    for download in download_one, download_many
+        yield check_cached, download
+def test_cached_with_date():
+    for download in download_one, download_many
+        yield check_cached_with_date, download
+def test_not_cached():
+    for download in download_one, download_many
+        yield check_not_cached, download
