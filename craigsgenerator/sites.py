217941734244a11465a5ae4b2f082f477039a9a5
Thomas Levine <_@thomaslevine.com>
2014-04-12 18:20:29 +0000

    fix sites maybe

--- craigsgenerator/sites.py
+++ craigsgenerator/sites.py
@@ -1,4 +1,8 @@
 import requests
+import lxml.html
+
+from craigsgenerator.download import download
+
 def sites(get = requests.get, url = 'http://sfbay.craigslist.org', cachedir = 'craigslist', id = 'rightbar'):
     '''
     Generate craigslist sites.
@@ -7,8 +11,8 @@ def sites(get = requests.get, url = 'http://sfbay.craigslist.org', cachedir = 'c
     results = set()
     warehouse = Warehouse(cachedir)
 
-    response = parse.download(warehouse, url, get)
-    html = parse.load_response(response)
+    response = download(get, warehouse, url, None)
+    html = lxml.html.fromstring(response.text)
 
     for href in html.xpath('id("%s")/descendant::a/@href' % id):
         p = urlparse(href.rstrip('/'))
