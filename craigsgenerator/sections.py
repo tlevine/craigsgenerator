b6e8efe14dcee344ff541c52ce8de0ae2cfdabbd
Thomas Levine <_@thomaslevine.com>
2014-04-12 19:01:27 +0000

    test

--- craigsgenerator/sections.py
+++ craigsgenerator/sections.py
@@ -1,13 +1,18 @@
+import warnings
+
 import lxml.html
 from craigsgenerator.download import download
 
 def _sections(get, warehouse, url):
     response = download(get, warehouse, url, None)
     html = lxml.html.fromstring(response.text)
-    all = list(map(str, html.xpath('id("main")/descendant::a/@href')))
-    simple = list(href.rstrip('/') for href in all if len(href) == 4)
-    complicated = list(filter(lambda href: len(href) != 4 and href.startswith('/i/'), all))
-    return simple, complicated
+    for href in map(str, html.xpath('id("main")/descendant::a/@href')):
+        if len(href) == 4:
+            yield href.rstrip('/')
+        elif href.startswith('/i/personals/'):
+            yield href.replace('/i/personals?category=','')
+        elif href.startswith('/i/'):
+            warnings.warn('Go to %s to see more sections.' % href)
 
 import requests
 from pickle_warehouse import Warehouse
